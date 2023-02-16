ef0158b67df599207b923021ede2788232abfcdb
Kang Hyejin <dreamfulbud@naver.com>
2023-02-16 22:13:26 +0900

    ♻️ state 정리, 순서정하기

--- ttt/src/components/Board.jsx
+++ ttt/src/components/Board.jsx
@@ -3,25 +3,12 @@ import styled from "styled-components";
 import Square from "./Square";
 
 function Board(props) {
-  const [state, setState] = useState({
-    squares: Array(9).fill(null),
-  });
-
-  const handleClick = (i) => {
-    const squares = state.squares.slice();
-    squares[i] = "X";
-    setState({
-      squares: squares,
-    });
-  };
+  const { squares, handleClick } = props;
+  console.log(props);
   return (
     <StyledDiv>
-      {state.squares.map((_, i) => (
-        <Square
-          key={i}
-          value={state.squares[i]}
-          onClick={() => handleClick(i)}
-        />
+      {squares.map((_, i) => (
+        <Square key={i} value={squares[i]} onClick={() => handleClick(i)} />
       ))}
     </StyledDiv>
   );
--- ttt/src/components/Game.jsx
+++ ttt/src/components/Game.jsx
@@ -3,11 +3,25 @@ import styled from "styled-components";
 import Board from "./Board";
 
 function Game() {
-  const status = "X";
+  const [state, setState] = useState({
+    squares: Array(9).fill(null),
+    xIsNext: true,
+  });
+
+  let status = state.xIsNext ? "X" : "O";
+
+  const handleClick = (i) => {
+    const squares = state.squares.slice();
+    squares[i] = state.xIsNext ? "X" : "O";
+    setState({
+      squares: squares,
+      xIsNext: !state.xIsNext,
+    });
+  };
 
   return (
     <StyledDiv>
-      <Board />
+      <Board {...state} handleClick={handleClick} />
 
       <section>
         <h2>게임정보</h2>
